8d242a6fcff5ad90f81c129c91346c5cddc9db20
niamul007 <am3994566@gmail.com>
2026-04-07 20:34:22 +0600

    adding company routes

---
+++ server/src/routes/company.routes.ts
@@ -0,0 +1,14 @@
+import * as company from '../controllers/company.controller';
+import { Router } from 'express';
+import  {authorize}  from '../middlewares/rbac.middleware';
+import { protect } from '../middlewares/auth.middleware';
+
+const router = Router();
+
+router.post('/', protect, authorize('admin', 'user'), company.create);
+router.get('/', company.getAll);
+router.post('/', protect, authorize('employer'), company.create);
+router.put('/:id', protect, authorize('employer'), company.update);
+router.delete('/:id', protect, authorize('employer'), company.remove);
+
+export default router;
