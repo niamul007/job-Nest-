8139f90594e085e9ee673b44ea9164d6a34d277f
niamul007 <am3994566@gmail.com>
2026-04-12 17:32:45 +0600

    adding socket to the servicer

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,14 @@
+import http from 'http';
 import app from './src/app';
 import { env } from './src/config/env';
 import './src/config/db';
 import './src/config/redis';
+import { initWebSocketServer } from './src/websocket/ws.server';
 
-app.listen(env.port, () => {
+const server = http.createServer(app);
+
+initWebSocketServer(server);
+
+server.listen(env.port, () => {
   console.log(`🚀 Server is running on port ${env.port}`);
 });
