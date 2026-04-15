e8bd2ab07824658ad1193aa04f906262f550b716
niamul007 <am3994566@gmail.com>
2026-04-15 17:45:26 +0600

    adding zod schemas

---
+++ server/src/middlewares/validate.middleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { ZodSchema } from "zod";
+
+export const validate = (schema: ZodSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+    if (!result.success) {
+      res.status(400).json({
+        success: false,
+        message: result.error.issues[0].message,
+      });
+      return;
+    }
+    next();
+  };
+};
