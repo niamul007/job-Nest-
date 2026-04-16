e440db2b27abd9e8111d72a2a9c9f67e3b47b010
niamul007 <am3994566@gmail.com>
2026-04-16 15:56:16 +0600

    adding auth validation and company

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import * as controller from '../controllers/auth.controller';
+import { validate } from "../middlewares/validate.middleware";
+import { registerSchema, loginSchema } from "../validators/auth.validator";
 const router = Router();
 
 
-router.post('/register',controller.register);
-router.post('/login', controller.login);
+router.post('/register', validate(registerSchema), controller.register);
+router.post('/login', validate(loginSchema), controller.login);
 
 export default router;
