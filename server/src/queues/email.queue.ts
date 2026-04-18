1f67d9e2672387e5905f26a0ba0f7a8deefc2270
niamul007 <am3994566@gmail.com>
2026-04-18 20:17:10 +0600

    hide important data and upgrate a bit

--- server/src/queues/email.queue.ts
+++ server/src/queues/email.queue.ts
@@ -1,5 +1,5 @@
 import Bull from "bull";
-import {env} from "../config/env";
+import { env } from "../config/env";
 
 const emailQueue = new Bull("email", {
   redis: {
@@ -7,7 +7,17 @@ const emailQueue = new Bull("email", {
     port: env.redis.port,
     username: env.redis.username,
     password: env.redis.password,
+  },
+  // ✅ add these - you were missing them
+  defaultJobOptions: {
+    attempts: 3,
+    backoff: {
+      type: 'exponential',
+      delay: 2000
+    },
+    removeOnComplete: true,
+    removeOnFail: false
   }
 })
 
-export default emailQueue;  
+export default emailQueue;
